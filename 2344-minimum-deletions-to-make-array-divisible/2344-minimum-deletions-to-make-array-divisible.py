54babaf3d9e091361a6c6d3360ab8a9c9c5b3cec
shilpabhoosaraddi <shilpabhoosaraddi5@gmail.com>
2026-02-04 14:56:06 +0530

    Time: 39 ms (37.17%), Space: 30.5 MB (25.66%) - LeetHub

---
+++ 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minOperations(self, nums: List[int], numsDivide: List[int]) -> int:
+        gcd = numsDivide[0]
+        for i in range(1,len(numsDivide)):
+            gcd = math.gcd(gcd,numsDivide[i])
+        nums.sort()
+        for i in range(len(nums)):
+            if gcd%nums[i]==0:
+                return i
+        return -1
+        
